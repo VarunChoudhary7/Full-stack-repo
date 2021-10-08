585debe9ad52d6a3e29d1fbcf6e22292735fe82e
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-08 20:58:40 +0530

    JS 2nd

--- app.js
+++ app.js
@@ -1,37 +1,12 @@
-// everything is considered an object in js 
-console.log("Hello world")
-// used to print something in console 
-console.warn("Hello warning")
-// used to give a warning 
-console.error("Hello error")
-// used to give an error
-// datatypes 
-// they are of two types primitive and non primitive 
-// primitive not derieved and vice versa 
-// in js objects are non primitve and any other thing is primitive 
-// everything in java script is object and hence a non primitive data type 
-// primitves we have still 
-// first is string 
-console.log(typeof "Hello word")
-// this tell us the type of data we are using 
-
-// this is still a string so there are no characters only strings in js
-
-// Numbers
-// just a single data type number for all numbers 
-console.log(typeof 23255)
-console.log(typeof 1)
-console.log(typeof 1.2632950)
-console.log(typeof -4165.546)
-console.log(typeof 645321564461654.546589432138)
-
-// boolean
-console.log(true)
-console.log(typeof false)
-
-
-
-
+// var age = 19 
+// var age = null 
+var age
+// we can assign a value of null to it 
+console.log(age)
+// a null means it have a value null is considered a value but undefined means that it do not have any value  
+console.log(Name)
+// interpreter don't know what the hell it is
+console.log(typeof age)
 
 
 
