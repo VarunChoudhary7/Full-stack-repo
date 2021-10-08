45c0c4169c32b0184e5c360f1f6b285ff3087bc0
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-08 21:04:48 +0530

    Js 3rd commit

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-// var age = 19 
-// var age = null 
-var age
-// we can assign a value of null to it 
-console.log(age)
-// a null means it have a value null is considered a value but undefined means that it do not have any value  
-console.log(Name)
-// interpreter don't know what the hell it is
-console.log(typeof age)
+var arr = [1, 2, 3, 4, "Age"]
+// arrays are not generic
+
+console.log(arr.length)
+arr.push(5)
+
+console.log(arr)
+console.log(typeof arr)
+console.log(arr.length)
+// the interpreter is moving line by line
+
 
 
 
