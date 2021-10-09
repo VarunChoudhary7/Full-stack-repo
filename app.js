265a0d22adb899fedcdb4d1fc2455321b2ca3088
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-10 03:30:50 +0530

    JS-4 3rd

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
 var arr = [1, 2, 3, 4, 5, 6, 7, 8]
 
-// arr.unshift(6)
+// arr.splice(3, 2)
+// first is index then the number of elements after that i want to remove 
+
+console.log(arr.splice(3, 2))
 // console.log(arr)
 
-
-// console.log(arr.unshift(6))
-// arr.shift()
-// console.log(arr)
-// shift deletes the first Element and unshits adds element at the first place
-
-// var new_arr = arr.slice(2, 5)
-// // this does not change the original array it only modifies it 
-// // 2 is inclusive while 5 is not 
-// console.log(new_arr)
-// var new_arr = []
-// for (var i = 0; i < arr.length; i++) {
-//     if (
-//         arr[i]
-//         % 2 === 0)
-//         new_arr.push(arr[i])
-// }
-
-// console.log(new_arr)
-
-var new_arr = arr.filter((element, index) => {
-    // console.log(element, index)
-    // if (element % 2 === 0) return true
-    if (element < 4) return true
-})
-console.log(new_arr)
 
 
 
