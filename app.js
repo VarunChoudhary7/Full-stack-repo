f7bcc674a3217271ced06706f6d07dc6dcbfdc81
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-10 03:05:16 +0530

    JS-4 one

--- app.js
+++ app.js
@@ -1,25 +1,78 @@
-var body = document.querySelector("body")
+const a = document.querySelectorAll("input")[0]
+const b = document.querySelectorAll("input")[1]
+const add = document.querySelector("#add")
+const multiply = document.querySelector("#multiply")
+const subtract = document.querySelector("#subtract")
+const resultBox = document.querySelector(".result")
 
-var button = document.querySelector("button")
+// const a = document.getElementsByTagName("input")[0]
+// const b = document.getElementsByTagName("input")[1]
+// console.log(a, b)
+// console.log(add, multiply)
+// console.log(result)
 
-
-
-
-
-
-// const callBackFunction = (happen) => {
-//     const inputValue = input.value
-//     const element = document.createElement("li")
-//     const textNode = document.createTextNode(inputValue)
-//     element.appendChild(textNode)
-//     list.appendChild(element)
-// }
-const callBackFunction = (happen) => {
-    // body.classList.add("dark")
-    body.classList.toggle("dark")
+const sum = () => {
+    const result = parseInt(a.value) + parseInt(b.value)
+    console.log(result)
+    resultBox.innerHTML = result
+    // resultBox.textContent = result
 
 }
 
 
 
-button.addEventListener("click", callBackFunction)
+const product = () => {
+    const result = parseInt(a.value) * parseInt(b.value)
+    console.log(result)
+    resultBox.innerHTML = result
+    // resultBox.textContent = result
+
+}
+
+// const calculate = (event, operation) => {
+const calculate = (event, operation) => {
+
+    // console.log(event.target)
+    // if (operation == "add") {
+    //     console.log("adding")
+    // }
+    // else if (operation == "multiply") {
+    //     console.log("multiplying")
+    // }
+    switch (operation) {
+        case "add":
+            resultBox.innerHTML = parseInt(a.value) + parseInt(b.value)
+            break
+        case "multiply":
+            resultBox.innerHTML = parseInt(a.value) * parseInt(b.value)
+            break
+        case "subtract":
+            resultBox.innerHTML = parseInt(a.value) - parseInt(b.value)
+            break
+
+        default:
+            resultBox.innerHTML = "Not a valid option"
+    }
+
+
+}
+
+
+
+// add.addEventListener("click", sum)
+// multiply.addEventListener("click", product)
+// add.addEventListener("click", calculate("add"))
+// in this we are doing a function call but we just need to pass the signature
+add.addEventListener("click", (event) => {
+    // console.log(multiplying)
+    // return calculate(add)
+    calculate(event, "add")
+})
+multiply.addEventListener("click", (event) => calculate(event, "multiply"))
+subtract.addEventListener("click", (event) => calculate(event, "subtract"))
+
+
+
+
+
+
