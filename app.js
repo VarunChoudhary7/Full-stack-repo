1bf16af37c0f888696a6b15674d7220830ada435
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-09 11:52:51 +0530

    JS-2 first

---
+++ app.js
@@ -0,0 +1,27 @@
+// Document object model
+// dom is an api
+var heading = document.getElementById('heading')
+heading.innerHTML = "Hi from JS"
+// this changes the content of the element 
+var para = document.getElementsByClassName("para")
+var tags = document.getElementsByTagName('a')
+var query = document.querySelector("#heading")
+// it uses css selectors 
+var hello = document.querySelector(".para")
+
+
+console.log(heading)
+console.log(typeof heading)
+console.log(para)
+console.log(para.length)
+
+console.log(para[0])
+console.log(para[1])
+
+
+// para.push("asdf")
+// getElementsByClassName does not give and array but it gives us an object something similar to an array
+
+console.log(tags)
+console.log(query)
+console.log(hello)
