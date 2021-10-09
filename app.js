77c989087a7bf3e999b7c8e06af15804875ecd12
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-09 22:11:44 +0530

    Js-3 3rd

--- app.js
+++ app.js
@@ -1,48 +1,16 @@
-// var arr = ["go to gym ", "cook", "eat food"]
-
-// for (var i = 0; i<arr.length; i++) {
-//     console.log(arr[i])
-// }
-// var callBackFunction = () => {
-//     console.log("I ran")
-// }
-// var callBackFunction = (elements, index) => {
-//     console.log(elements, index)
-// }
-
-
-// arr.forEach(callBackFunction)
-// arr.forEach((elements, index) => {
-//     console.log(elements, index)
-// }
-// )
-
 var button = document.querySelector("button")
 var input = document.querySelector("input")
 var list = document.querySelector("ul")
 
-console.log(button, input, list)
-
-
-// const callBackFunction = () => {
-//     console.log("sdf")
-// }
 
 
 
-// button.addEventListener("click", () => {
-//     console.log("sdf")
-// })
 
-// const callBackFunction = (mystery) => {
-//     console.log("sdf", mystery)
-// }
-
-// const callBackFunction = (event) => {
-//     console.log(event.target)
-//     console.log(input.value)
-// }
-
-
-
+const callBackFunction = (happen) => {
+    const inputValue = input.value
+    const element = document.createElement("li")
+    const textNode = document.createTextNode(inputValue)
+    element.appendChild(textNode)
+    list.appendChild(element)
+}
 button.addEventListener("click", callBackFunction)
