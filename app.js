8c67fbe711f6a6e42353265fbb6baecfc567a9db
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-09 22:21:05 +0530

    JS-3 4th

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
+var body = document.querySelector("body")
+
 var button = document.querySelector("button")
-var input = document.querySelector("input")
-var list = document.querySelector("ul")
 
 
 
 
 
+
+// const callBackFunction = (happen) => {
+//     const inputValue = input.value
+//     const element = document.createElement("li")
+//     const textNode = document.createTextNode(inputValue)
+//     element.appendChild(textNode)
+//     list.appendChild(element)
+// }
 const callBackFunction = (happen) => {
-    const inputValue = input.value
-    const element = document.createElement("li")
-    const textNode = document.createTextNode(inputValue)
-    element.appendChild(textNode)
-    list.appendChild(element)
+    // body.classList.add("dark")
+    body.classList.toggle("dark")
+
 }
+
+
+
 button.addEventListener("click", callBackFunction)
