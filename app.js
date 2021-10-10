bba73f5f7103d524242b8cea5c0988f929ef233d
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-10 15:08:32 +0530

    JS-5 1st

--- app.js
+++ app.js
@@ -1,10 +1,91 @@
-var arr = [1, 2, 3, 4, 5, 6, 7, 8]
+// let a = 20
+// var b = 10
 
-// arr.splice(3, 2)
-// first is index then the number of elements after that i want to remove 
+// console.log(a, b)
 
-console.log(arr.splice(3, 2))
-// console.log(arr)
+// a = 23
+// b = 54
+// console.log(a, b)
+
+// if (true) {
+//     var Name = "ferrari"
+//     console.log(Name)
+// }
+
+// console.log(Name)
+
+// if (true) {
+//     let Name = "ferrari"
+//     console.log(Name)
+// }
+
+// console.log(Name)
+
+// this shows that there is a diff between let and variable
+
+// if (true) {
+//     // let Name = "ferrari"
+//     var Name = "ferrari"
+
+// }
+// if (true) {
+//     console.log(Name)
+// }
+
+// console.log(Name)
+
+// // for (var i = 0; i < 10; i++) {
+// for (let i = 0; i < 10; i++) {
+//     // console.log("loop ran")
+// }
+
+// console.log(i)
+
+
+// if (true) {
+//     // var Name = "ferrari"
+//     let Name = "ferrari"
+//     if (true) {
+//         console.log(Name)
+//     }
+// }
+
+// if (true) {
+//     console.log(Name)
+//     if (true) {
+//         var Name = "ferrari"
+//     }
+// }
+
+// a child can access a parent but a parent can't access a child  
+
+const print = () => {
+    var Name = "mercedes"
+
+    if (true) {
+        console.log(Name)
+    }
+    return Name
+
+}
+
+// in fuction no global scope occur
+// print() 
+// console.log(Name)
+// const is a block scope similar to a let 
+
+print()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
