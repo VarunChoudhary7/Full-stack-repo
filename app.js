b1849f1e06841eaf6f75cc32abd31f23d34137f7
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-16 16:43:29 +0530

    JS-6 1st

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
-var data = fetch("https://jsonplaceholder.typicode.com/todos/10")
+// console.log("one")
+// console.log("two")
+// setTimeout(() => {
+//     console.log("timer 2")
+// }, 1000)
+// setTimeout(() => {
+//     console.log("timer 1")
+// }, 2000)
+// console.log("three")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
+// console.log("four")
 
-console.log(data)
+// setTimeout(() => {
+//     console.log("Hi")
+// }, 0)
+// for (var i = 0; i < 90000; i++) {
+//     console.log("HI")
+// }
+
+// set timeout is the minimum delay we can create 
+
 
 
 
