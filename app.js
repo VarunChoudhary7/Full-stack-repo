606412552621dd784279f340bf0034742d204a2a
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-17 22:46:58 +0530

    Js-7 first

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
-// let value = new Promise((resolve, rejection) => {
+// const hello = new Promise((resolve, reject) => {
 //     setTimeout(() => {
-//         resolve("Hello")
-//         // resolve and reject are not keyowrds we can use anything we want but  the order needs to remain the same
-//         // rejection("Hello") 
-//     }, 3000)
+//         resolve("My name is Varun")
+//     }, 3000
+//     )
+
 // })
+// // console.log(hello)
+// // setTimeout(() => {
+// //     console.log(hello)
 
-// console.log(value)
+// // }, 4000)
 
-// setTimeout(() => {
-//     console.log(value)
-// }, 4000)
-
-// let response = fetch("https://jsonplaceholder.typicode.com/users")
-// console.log(response)
-// let response = fetch("https://jsonplaceholder.typicode.com/users").then(data => { console.log(data) })
-// this is an object not a proper response 
-
-// .json() comibes a data stream it is not an array it shows all the data present in a stream 
-
-// let response = fetch("https://jsonplaceholder.typicode.com/users").then(res => {
-//     // console.log(res.json())
-//     // we can see that in turn this also provides a promise 
-//     let parsedData = res.json()
-//     console.log(parsedData)
-// }).then(data => { console.log(data) })
-// // this .then always return a promise
-// // all the pomises are thenable 
+// hello.then(name => { console.log(name) })
 
 
-// let response = fetch("https://jsonplaceholder.typicode.com/users").then(res => {
-//     let parsedData = res.json()
-//     console.log(parsedData)
-//     return parsedData
+// the task to display all the data after fetching from the url 
+ul = document.querySelector("ul");
+// console.log(ul);
+let response = fetch("https://jsonplaceholder.typicode.com/users").then(resp => resp.json()).then(data => {
+    console.log(data);
+    append_data = (element) => {
+        lielement = document.createElement("li");
+        user_detail = element['name'] + element['username'] + element['email']
+        textnode = document.createTextNode(user_detail);
+        lielement.appendChild(textnode);
+        ul.appendChild(lielement);
+    }
+    data.forEach(append_data);
 
-//     // this return makes the promise return something as if we don't return something it will pass on nothing 
-//     // json is a java script object it converts an array into an object 
-
-// }).then(data => { console.log(data) })
-
-let response = fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then(data => { console.log(data) })
+})
