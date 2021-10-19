b3065a8b7ad29b8d01d864225ad427c2c3bcc0bd
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-19 14:10:43 +0530

    Js-7 3rd

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-// ul = document.querySelector("ul");
-// // console.log(ul);
-// let response = fetch("https://jsonplaceholder.typicode.com/users").then(resp => resp.json()).then(data => {
-//     console.log(data);
-//     append_data = (element) => {
-//         lielement = document.createElement("li");
-//         user_detail = element['name'] + element['username'] + element['email']
-//         textnode = document.createTextNode(user_detail);
-//         lielement.appendChild(textnode);
-//         ul.appendChild(lielement);
-//     }
-//     data.forEach(append_data);
+// const table = document.querySelector("table")
 
-//}
-
-const table = document.querySelector("table")
-
-console.log(table)
+// console.log(table)
 
 
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    // .then(data => console.log(data))
-    .then(data => {
-        data.forEach(user => {
-            // console.log(user)
-            let newRow = document.querySelector("tr")
-            let dataId = document.querySelector("td")
-            let dataIdTextNode = document.createTextNode(user.id)
-            dataId.appendChild(dataIdTextNode)
-            newRow.appendChild(dataId)
+// fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then(data => {
+//     data.forEach(user => {
+//         console.log(user)
+//         let newRow = document.querySelector("tr")
+//         let dataid = document.querySelector("td")
+//         let dataidTextNode = document.createTextNode(user.id)
+//         dataid.appendChild(dataidTextNode)
+//         newRow.appendChild(dataid)
 
-            table.appendChild(newRow)
-        });
-    })
+//         table.appendChild(newRow)
+//     })
+// })
+
+// alternate method 
+
+// const table = document.querySelector("table")
+// console.log(table)
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(res => res.json())
+//     .then(data => {
+//         data.forEach((user) => {
+//             const { id, name, email, username } = user;
+//             new_row = document.createElement("tr");
+//             new_row.innerHTML = `<td>${id}</td>
+//                                    <td>${name}</td>
+//                                    <td>${email}</td>
+//                                    <td>${username}</td>`
+//             table.appendChild(new_row);
+//         })
+//     })
+
+
+
