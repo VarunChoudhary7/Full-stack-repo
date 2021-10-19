4f2082dce432584aa46393cfb99e35c545c8c1e6
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-19 17:49:02 +0530

    Js-7 5th

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const table = document.querySelector("table")
 console.log(table)
 
-
 fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json())
     .then(data => {
         console.log(data)
         data.forEach(user => {
-            // console.log(user)
             const newRow = document.createElement('tr')
-            const dataId = document.createElement('td')
-            const dataIdTextNode = document.createTextNode(user.id)
-            dataId.appendChild(dataIdTextNode)
-            newRow.appendChild(dataId)
+            let element = `<td>${user.id}</td>
+                            <td>${user.name}</td>
+                            <td>${user.email}</td>
+                            <td>${user.username}</td>`
+
+            // console.log(element)
+            newRow.innerHTML = element
+
+            // newRow.appendChild(element)
 
             table.appendChild(newRow)
-            // now if we want to add name and everything we will just clutter the code as we need to do this again and again 
-            // so sir will teach an alternate method 
+
         });
 
     })
