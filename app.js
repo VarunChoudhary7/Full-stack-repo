f83bd438ae8a1562cc26a2832e5d9bf1fc9dcd7d
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-19 14:33:12 +0530

    JS-7 4th

--- app.js
+++ app.js
@@ -1,38 +1,21 @@
-// const table = document.querySelector("table")
-
-// console.log(table)
-
-
-// fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json()).then(data => {
-//     data.forEach(user => {
-//         console.log(user)
-//         let newRow = document.querySelector("tr")
-//         let dataid = document.querySelector("td")
-//         let dataidTextNode = document.createTextNode(user.id)
-//         dataid.appendChild(dataidTextNode)
-//         newRow.appendChild(dataid)
-
-//         table.appendChild(newRow)
-//     })
-// })
-
-// alternate method 
-
-// const table = document.querySelector("table")
-// console.log(table)
-// fetch("https://jsonplaceholder.typicode.com/users")
-//     .then(res => res.json())
-//     .then(data => {
-//         data.forEach((user) => {
-//             const { id, name, email, username } = user;
-//             new_row = document.createElement("tr");
-//             new_row.innerHTML = `<td>${id}</td>
-//                                    <td>${name}</td>
-//                                    <td>${email}</td>
-//                                    <td>${username}</td>`
-//             table.appendChild(new_row);
-//         })
-//     })
+const table = document.querySelector("table")
+console.log(table)
 
 
+fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(user => {
+            // console.log(user)
+            const newRow = document.createElement('tr')
+            const dataId = document.createElement('td')
+            const dataIdTextNode = document.createTextNode(user.id)
+            dataId.appendChild(dataIdTextNode)
+            newRow.appendChild(dataId)
 
+            table.appendChild(newRow)
+            // now if we want to add name and everything we will just clutter the code as we need to do this again and again 
+            // so sir will teach an alternate method 
+        });
+
+    })
