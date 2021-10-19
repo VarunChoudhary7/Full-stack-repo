ca1d43bcb657c0dd669e5d09a6f0365b6ecd8a0f
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-19 17:53:53 +0530

    JS-7 6th

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const table = document.querySelector("table")
 console.log(table)
 
+const createRow = (user) => {
+    let newRow = document.createElement('tr')
+    let element = `<td>${user.id}</td>
+                    <td>${user.name}</td>
+                    <td>${user.email}</td>
+                    <td>${user.username}</td>`
+    newRow.innerHTML = element
+    return newRow
+}
+
+
 fetch("https://jsonplaceholder.typicode.com/users").then(res => res.json())
     .then(data => {
         console.log(data)
         data.forEach(user => {
-            const newRow = document.createElement('tr')
-            let element = `<td>${user.id}</td>
-                            <td>${user.name}</td>
-                            <td>${user.email}</td>
-                            <td>${user.username}</td>`
 
-            // console.log(element)
-            newRow.innerHTML = element
-
-            // newRow.appendChild(element)
-
-            table.appendChild(newRow)
+            table.appendChild(createRow(user))
 
         });
 
