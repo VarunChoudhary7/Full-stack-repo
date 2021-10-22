28c965d9db48febeda922275c4f813a4978802d6
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-22 16:55:36 +0530

    NJS-1 first

---
+++ index.js
@@ -0,0 +1,39 @@
+// console.log("something")
+
+// for (var i = 0; i < 10; i++) {
+//     console.log('iteration', i + 1)
+// }
+
+// console.log(document)
+// it will throw an eror as there is no dom bcoz it was created by the browser
+
+// console.log(window)
+// it is also undefined 
+
+// setTimeout(() => {
+//     console.log('hello')
+// }, 2000)
+// this is provide to us by V8 engine so it works fine without any error
+
+
+// a module is just a file in which we have defined our code seperaely and are importing and exporting our code from there for use 
+// it just makes our code more organised 
+// console.log(module)
+// only node js supports these modules chrome will throw an error 
+
+const greet = () => "hi"
+
+// module.exports.something = 'hi'
+// module.exports['something'] = 'hi'
+module.exports.greet = greet
+
+console.log(module)
+
+
+
+
+
+
+
+
+
