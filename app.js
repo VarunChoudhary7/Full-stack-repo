38256c179097c8cbd37bdd6f9adc6396323fe995
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-23 16:03:24 +0530

    NJS-1 3rd'

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// console.log(module)
-// console.log(require('./index'))
-// require gives the access of exports object 
-// require returns only the exports object smjhe
-// now we get to know that it does not only gives you access to the exports object it also executes you file 
+// const stuff = require('./utils')
+const { add, subtract, multiply, divide, message } = require('./utils')
+// console.log(stuff)
 
+add()
+subtract()
+multiply()
+divide()
 
-
-const stuff = require('./index')
-
-console.log(stuff)
+console.log(message)
