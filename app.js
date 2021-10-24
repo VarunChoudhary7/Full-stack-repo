5d41f2bb6ee816cb85ee4ff1e89c1fe713fb245c
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-24 14:40:31 +0530

    NJS-2/3

--- app.js
+++ app.js
@@ -2,17 +2,44 @@ const button = document.querySelector('button')
 const input = document.querySelector('input')
 const div = document.querySelector('div')
 
-button.addEventListener('click', (req, res) => {
-    div.innerHTML = input.value
-    // div.innerHTML = input
-    // inner Html expects a string nothing else 
-})
+// button.addEventListener('click', (req, res) => {
 
-// local storage is just a storage at my browser and it stores my data 
+//     div.innerHTML = input.value
+
+// })
+
+// console.log(localStorage)
+
+// localStorage.setItem('Inputvalue', "Something"
+// u can comment it first now the value will still stay 
+// console.log(localStorage.getItem('Inputvalue'))
+
+// localStorage.clear()
+
+// let last = localStorage.getItem('inputValue')
+// div.innerHTML = last
+// div.innerHTML = localStorage.getItem('inputValue')
 
 
+// button.addEventListener('click', (req, res) => {
+//     // localStorage.setItem('inputValue', input.value)
+//     localStorage.setItem('inputValue', [1, 2, 3, 4])
 
+//     div.innerHTML = input.value
+// })
 
+// const arr = localStorage.getItem('inputValue')
+// console.log(typeof arr)
+// localStorage.clear()
+const arr = [1, 2, 3, 4, 5]
+// JSON.stringify(arr)
 
+// console.log(arr)
+// localStorage.setItem('val',arr)
 
+localStorage.setItem('val', JSON.stringify(arr))
+// JSON.stringify converts an array to a string 
 
+// console.log(localStorage.getItem('val'))
+console.log(JSON.parse(localStorage.getItem('val')))
+// JSON.parse converts the same string back to an array after fetching it
