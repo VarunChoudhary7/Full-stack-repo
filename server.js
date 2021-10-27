92523ebc1d3768d5a6c5ff8117c4dc3f49e8e932
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-28 00:18:38 +0530

    NJ-6 3rd

--- server.js
+++ server.js
@@ -6,32 +6,37 @@ app.use(express.json())
 
 let products = [{ name: "iPhone 12 case", price: "999" }, { name: "iPhone 13 case", price: "1199" }, { name: "iPhone 13 pro case", price: "1499" }]
 
+// MIDDlEWARE
+const validator=(req,res,next)=>{
+    
+}
+
+
 //--------------PUBLIC Route------------ 
 //this route is a get route 
 //this will send products as response
 app.get('/products', (req, res) => {
-    // res.send(products)
     res.json({ products })
-    // after this we don't need to parse it it will do that automatically
-
 })
 
 //--------------PRIVATE Route------------ 
 
 app.post('/products/add', (req, res) => {
-    // console.log(req.body)
     const { name, price } = req.body
-    const newProduct = {
-        // name: req.body.name,
-        // name: name,
-        name,
-        // this works the same way the above command do because es6 allows you to do this 
-        // price: req.body.price,
-        // price: price,
-        price,
+    // const newProduct = {
+    //     name,
+    //     price,
+    // }
+
+    if (!name || !price) res.send({ error: "Validation Failed" })
+    else {
+
+        products.push({
+            name,
+            price,
+        })
+        res.send({ products })
     }
-    products.push(newProduct)
-    res.send('product added')
 })
 
 
