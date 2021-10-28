81ed8dc8dd9357f19a4287c29ebb678934c0add3
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-29 01:25:26 +0530

    first commit in special class

--- server.js
+++ server.js
@@ -1,56 +1,24 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
-
-app.use(express.json())
-
-const token = "TOP_SECRET"
-
-let products = [{ name: "iPhone 12 case", price: "999" }, { name: "iPhone 13 case", price: "1199" }, { name: "iPhone 13 pro case", price: "1499" }]
 
 
-// MIDDlEWARE
-const validator = (req, res, next) => {
-    const { name, price } = req.body
-    if (!name || !price) res.json({ error: "Validation Failed" })
-    else next()
-}
+app.get('/products/cases')
+app.get('/products/protection')
+app.get('/products/bands')
 
-const isAuthorised = (req, res, next) => {
-    if (req.headers.authorisation === token) next()
-    else res.json({ error: "Authorization Failed" })
-}
+app.get('/products/:categories', (req, res) => {
+    if (req.params.categories = 'iPhone')
+        if (req.params.categories = 'mac')
+            if (req.params.categories = 'iPad')
+                if (req.params.categories = 'music')
 
 
-//--------------PUBLIC Route------------ 
-app.get('/products', (req, res) => {
-    res.json({ products })
-})
-
-//--------------PRIVATE Route------------ 
-
-app.post('/products/add', isAuthorised, validator, (req, res) => {
-    const { name, price } = req.body
-
-    products.push({
-        name,
-        price,
-    })
-    res.send({ products })
+                    console.log(req.params)
+    res.send("PING")
 })
 
 
-//-----------AUTHENTICATION Route----------
-app.post('/products/auth', (req, res) => {
-    const { email, password } = req.body
-    if (email === "admin@mail.com" && password === "password") {
-        res.send({ token })
-    } else {
-        res.send({ message: "UNAUTHORISED" })
-    }
-})
 
-
-app.listen(PORT, () => {
-    console.log(`Server is running at port: ${PORT} `)
+app.listen(3001, () => {
+    console.log("Listening at port 3001")
 })
