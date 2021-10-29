0c6393f28a2af552b646f43e547d429aa57c6672
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-29 22:56:30 +0530

    last commit in special class

---
+++ database/schema/schema.js
@@ -0,0 +1,19 @@
+// product
+/*
+id: id
+Name : string
+price: Number
+category: category_id
+*/
+
+// category
+/*
+id: id
+Name : string
+*/
+
+// order
+/*
+total price: Number
+product: array of products [product id]
+*/
--- server.js
+++ server.js
@@ -1,24 +1,11 @@
 const express = require('express')
 const app = express()
-
-
-app.get('/products/cases')
-app.get('/products/protection')
-app.get('/products/bands')
-
-app.get('/products/:categories', (req, res) => {
-    if (req.params.categories = 'iPhone')
-        if (req.params.categories = 'mac')
-            if (req.params.categories = 'iPad')
-                if (req.params.categories = 'music')
-
-
-                    console.log(req.params)
-    res.send("PING")
-})
+const database = require('./database/db')
+app.use(express.json())
 
 
 
 app.listen(3001, () => {
     console.log("Listening at port 3001")
 })
+
