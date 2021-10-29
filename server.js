52a0d1a42c5ea5b616b65f9c3666330e246fc024
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-30 01:57:28 +0530

    NJS-7 2nd

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
-// const express = require('express')
-// const app = express()
-// const database = require('./database/db')
-// app.use(express.json())
+const express = require('express')
+const app = express()
+const database = require('./database/db')
+app.use(express.json())
+const categoryRoutes = require('./routes/categoryRoutes')
 
-
-
-// app.listen(3001, () => {
-//     console.log("Listening at port 3001")
+// app.get('/', (req, res) => {
+//     // res.send('hi')
+//     console.log(document)
+//     res.send(error.message)
 // })
+console.log(categoryRoutes.stack)
 
-let data = ['one', 'two']
-let data = []
-let data = null
- 
+// app.use('/', categoryRoutes)
+app.use('/categories', categoryRoutes)
+// app.use('/mercedes', categoryRoutes)
+// app.use('/mercedes/1/2/3', categoryRoutes)
 
-data.forEach(item => {
-    console.log(item)
+// this is a new middleware and it uses to parameters first is the url it will be using and adding the route to it 
+
+app.get('/', (req, res) => {
+    try {
+        console.log(document)
+    } catch (error) {
+        res.status(200).send(error.message)
+    }
+})
+
+app.listen(3001, () => {
+    console.log("Listening at port 3001")
 })
