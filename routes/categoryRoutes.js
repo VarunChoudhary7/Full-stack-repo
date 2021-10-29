f9f1bffe28f206f657e11fd641aa6cf99486a5fb
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-30 00:01:32 +0530

    NJS-7 1st

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -4,21 +4,16 @@ const router = express.Router()
 
 router.get('/all', (req, res) => {
 
-
-    // it will first try this try statement block and then it will move on to the catch statement block if the try block do not work 
-    // it will also pass along the error as the parameter suggests error 
     try {
         res.json({
             categories: database.categories,
-            message: "Successfully fetched categories"
+            message: "Successfully fetched categories",
             status: "SUCCESS "
         })
-
-
     } catch (error) {
         res.json({
             categories: [],
-            message: error.message
+            message: error.message,
             status: "FAILED"
         })
     }
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const database = require('./database/db')
-app.use(express.json())
+// const express = require('express')
+// const app = express()
+// const database = require('./database/db')
+// app.use(express.json())
 
 
 
-app.listen(3001, () => {
-    console.log("Listening at port 3001")
+// app.listen(3001, () => {
+//     console.log("Listening at port 3001")
+// })
+
+let data = ['one', 'two']
+let data = []
+let data = null
+ 
+
+data.forEach(item => {
+    console.log(item)
 })
-
