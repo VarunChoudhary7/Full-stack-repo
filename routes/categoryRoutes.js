8cb537bd7a0565f75fc917d333c79eafe1f84a48
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-31 00:42:24 +0530

    NJS-8 1st'

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,18 +1,19 @@
 const database = require('../database/db')
 const express = require('express')
 const router = express.Router()
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
+const db = require('../database/db')
 
 router.get('/all', (req, res) => {
 
     try {
-        res.status(200).json({
+        res.json({
             categories: database.categories,
             message: "Successfully fetched categories",
             status: "SUCCESS "
         })
     } catch (error) {
-        res.status(200).json({
+        res.json({
             categories: [],
             message: error.message,
             status: "FAILED"
@@ -22,21 +23,25 @@ router.get('/all', (req, res) => {
 })
 
 router.post('/add', (req, res) => {
+    const { name } = req.body
+    const newCategory = {
+        id: uuidv4(),
+        name
+    }
+    // if(){}
+    let includes = database.categories.find(item => item.name === name)
+    console.log(includes)
+    // this find method just iteratees over each element and checks if we have it already or not 
+    database.categories.push(newCategory)
     try {
-        const { name } = req.body
-        let newCategory = {
-            name,
-            id: uuidv4
-        }
-        database.categories.push(newCategory)
-        res.status(200).json({
-            categories: categories,
-            message: error.message,
-            status: "SUCESS"
+        res.json({
+            categories: database.categories,
+            message: "Successfully added categories",
+            status: "SUCCESS"
         })
-
-    } catch (error) {
-        res.status(200).json({
+    }
+    catch (error) {
+        res.json({
             categories: [],
             message: error.message,
             status: "FAILED"
@@ -44,5 +49,5 @@ router.post('/add', (req, res) => {
     }
 })
 
-
 module.exports = router
+
--- server.js
+++ server.js
@@ -1,30 +1,11 @@
 const express = require('express')
 const app = express()
-const database = require('./database/db')
 app.use(express.json())
 const categoryRoutes = require('./routes/categoryRoutes')
 
-// app.get('/', (req, res) => {
-//     // res.send('hi')
-//     console.log(document)
-//     res.send(error.message)
-// })
-console.log(categoryRoutes.stack)
 
-// app.use('/', categoryRoutes)
 app.use('/categories', categoryRoutes)
-// app.use('/mercedes', categoryRoutes)
-// app.use('/mercedes/1/2/3', categoryRoutes)
 
-// this is a new middleware and it uses to parameters first is the url it will be using and adding the route to it 
-
-app.get('/', (req, res) => {
-    try {
-        console.log(document)
-    } catch (error) {
-        res.status(200).send(error.message)
-    }
-})
 
 app.listen(3001, () => {
     console.log("Listening at port 3001")
