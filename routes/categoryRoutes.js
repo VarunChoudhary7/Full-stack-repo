1521d25a59a337f442734e965e55d368cad2a29a
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-10-31 02:26:57 +0530

    NJS-8 2nd

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -29,11 +29,14 @@ router.post('/add', (req, res) => {
         name
     }
     // if(){}
-    let includes = database.categories.find(item => item.name === name)
-    console.log(includes)
+
+    // console.log(includes)
     // this find method just iteratees over each element and checks if we have it already or not 
-    database.categories.push(newCategory)
+
     try {
+        let includes = database.categories.find(item => item.name === name)
+        if (!includes) database.categories.push(newCategory)
+        else console.log("Already exists")
         res.json({
             categories: database.categories,
             message: "Successfully added categories",
@@ -49,5 +52,33 @@ router.post('/add', (req, res) => {
     }
 })
 
-module.exports = router
+// router.delete('/delete', (req, res) => {
+router.delete('/delete/:id', (req, res) => {
 
+    try {
+
+        // const { id } = req.body
+        const { id } = req.params
+
+        // let element = database.categories.find(item => item.id === id)
+        // const index = database.categories.indexOf(element)
+        // database.categories.splice(index, 1)
+        const newCategories = database.categories.filter(item => item.id !== id)
+        database.categories = newCategories
+
+        res.json({
+            categories: newCategories,
+            message: "Successfully removed categories",
+            status: "SUCCESS"
+        })
+    }
+    catch (error) {
+        console.log(error)
+        res.json({
+            categories: [],
+            message: error.message,
+            status: "FAILED"
+        })
+    }
+})
+module.exports = router
