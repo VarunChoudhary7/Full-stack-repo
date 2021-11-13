b62d176d2e567496c8423c750745e3795f403adb
VarunChoudhary7 <varunchoudhary828@gmail.com>
2021-11-13 17:57:25 +0530

    First commit

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 const container = document.getElementById("app")
 
-// console.log(container)
-
-// const headingone = React.createElement('h2', {}, 'This is a heading')
-
-// const headingtwo = React.createElement('h3', {}, 'This is heading three')
-
-
-// ReactDOM.render(headingone, container)
-// ReactDOM.render(headingtwo, container)
-
-// there can only be one render method multiple are not allowed
-
+// const content = React.createElement('h1', {}, 'hi')
 const content = React.createElement('div', {}, [
-    React.createElement('h1', {}, `I am heading one`),
-    React.createElement('p', {}, `I am paragraph one`),
-    React.createElement('p', {}, React.createElement('p', {}, `I am secondary paragraph`),)
-    // nesting
-]
-)
+    // React.createElement("h1", {}, "I am a heading"),
+    // React.createElement("h2", {}, "I am a heading"),
+    // React.createElement("h3", {}, "I am a heading")
+
+    // React.createElement("li", {}, "I am a heading"),
+    // React.createElement("li", {}, "I am a heading"),
+    // React.createElement("li", {}, "I am a heading")
+
+    React.createElement("li", {}, "I am a heading"),
+    React.createElement("li", {}, "I am a heading"),
+    React.createElement("ul", {}, [
+
+        React.createElement("li", {}, "Content"),
+        React.createElement("li", {}, "Content"),
+        React.createElement("li", {}, "Content"),
+        React.createElement("img", { src="" }, ""),
+    ])
+
+])
+
+// We need a transpilation app to convert this  into above code 
+
+const Name = "Varun"
+
+const jsx =
+    <ul>
+        <h2>{Name}</h2>
+        <li>I am heading</li>
+        <li>I am heading</li>
+        <li>I am heading</li>
+        <ul>
+            <li>Stuff</li>
+            <li>Stuff</li>
+        </ul>
+    </ul>
+
+
+
 
 ReactDOM.render(content, container)
